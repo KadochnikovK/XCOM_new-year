f6a5c81cffc653b2be5b1e5530ef60bc18a9e653
kadochnikovk <kadochnikov_k@mail.ru>
2025-11-12 23:59:25 +0500

    added all blocks

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -25,4 +25,31 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
+
+   const swiperFilum = new Swiper(".filumGoods", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 1.5,
+        spaceBetween: 30,
+      },
+       1024: {
+        slidesPerView: 2.5,
+        spaceBetween: 30,
+      },
+       1500: {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+      },
+    },
+  });
 });
